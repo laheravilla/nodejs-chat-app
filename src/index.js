89238175b4e8512f92158eeaf8c813d24ccdb5ca
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-08-17 07:05:12 +0200

    getting started with Socket.io

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const server = http.createServer(app); // Create a separated new server
+const io = socketio(server);
 
+const port = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirPath));
-app.listen(port, () => console.log('Server is up on port ' + port));
+
+io.on('connection', () => console.log('New WebSocket connection'));
+
+server.listen(port, () => console.log('Server is up on port ' + port));
