6d0aaeee872afc9c88a8c8a56925d239395dab06
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-08-18 06:49:21 +0200

    sharing location with gelocation api

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,11 +2,29 @@
 
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count);
+socket.on('message', (msg) => console.log(msg));
+
+const form = document.getElementById('send-message-form');
+
+form.addEventListener('submit', E => {
+    E.preventDefault();
+    let msg = Event.target.elements.message.value;
+
+    socket.emit('sendMsg', msg);
 });
 
-document.getElementById('js-increment').addEventListener('click', Event => {
-    console.log('Clicked!');
-    socket.emit('increment');
+document.getElementById('share-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.');
+    }
+
+    /** @inheritDoc on https://developer.mozilla.org/fr/docs/Web/API/Geolocation_API */
+    navigator.geolocation.getCurrentPosition((location) => {
+        location = {
+            longitude: location.coords.longitude,
+            latitude: location.coords.latitude
+        };
+
+        socket.emit('shareLocation', location);
+    });
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const path = require('path');
 const http = require('http');
 const express = require('express');
@@ -12,15 +14,25 @@ const publicDirPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirPath));
 
-let count = 0;
+let msg = "Welcome to our app ";
 
+// On connection event
 io.on('connection', (socket) => {
     console.log('New WebSocket connection');
-    socket.emit('countUpdated', count);
-    socket.on('increment', () => {
-        // socket.emit('countUpdated', count++); // Emits to a specific connection
-        io.emit('countUpdated', count++); // Emits to all connections
-    });
+
+    // Server emits to a single client connection
+    socket.emit('message', msg);
+
+    // Server emits to everybody except for the current connection
+    socket.broadcast.emit('message', 'A new user has joined!');
+
+    // Server emits to all client connections
+    socket.on('sendMsg', (msg) => io.emit('message', msg));
+
+    socket.on('shareLocation', (location) => io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`))
+
+    // On disconnection
+    socket.on('disconnect', () => io.emit('message', 'A user has left!'));
 });
 
 server.listen(port, () => console.log('Server is up on port ' + port));
