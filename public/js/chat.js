0f1cc23eb5a22bfd77c16fd54f0b98d468145b5d
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-08-19 06:05:22 +0200

    manage form and buttons

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,13 +2,33 @@
 
 const socket = io();
 
+// Elements
+const messageForm = document.getElementById('send-message-form');
+const messageFormTextarea = messageForm.elements.message;
+const messageFormBtn = messageForm.elements.btnSend;
+
+const resetMessage = () => {
+    messageFormTextarea.value = '';
+    messageFormBtn.setAttribute('disabled', 'disabled'); // Disable button once submitted
+};
+
 socket.on('message', (msg) => console.log(msg));
 
-const form = document.getElementById('send-message-form');
+resetMessage();
 
-form.addEventListener('submit', E => {
-    E.preventDefault();
-    let msg = E.target.elements.message.value;
+messageFormTextarea.addEventListener('keyup', e => {
+    console.log(e.currentTarget.value)
+    if (e.currentTarget.value !== '') {
+        messageFormBtn.removeAttribute('disabled'); // Enable button
+    } else {
+        resetMessage();
+    }
+});
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let msg = e.target.elements.message.value;
 
     /**
      * Event sendMsg
@@ -16,12 +36,18 @@ form.addEventListener('submit', E => {
      * Event Acknowledgements
      */
     socket.emit('sendMsg', msg, (error) => {
+        resetMessage()
+        messageFormTextarea.focus();
+
         if (error) return console.log(error);
         console.log('Message delivered!');
     });
 });
 
-document.getElementById('share-location').addEventListener('click', () => {
+document.getElementById('share-location').addEventListener('click', (e) => {
+    const shareLocationBtn = e.currentTarget;
+    shareLocationBtn.setAttribute('disabled', 'disabled');
+
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.');
     }
@@ -33,6 +59,10 @@ document.getElementById('share-location').addEventListener('click', () => {
             latitude: location.coords.latitude
         };
 
-        socket.emit('shareLocation', location, (message) => console.log(message));
+        socket.emit('shareLocation', location, (message) => {
+            shareLocationBtn.removeAttribute('disabled'); // Enable button
+
+            console.log(message);
+        });
     });
 });
