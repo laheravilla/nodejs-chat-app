c8c0c094bcdf3adc9a55239146fcb78feb294ec6
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-08-22 06:48:35 +0200

    automatic scroll

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,13 +9,36 @@ const messageFormBtn = messageForm.elements.btnSend;
 const messagesOutput = document.getElementById('messages-output');
 
 // Templates
-const messageTemplate = document.getElementById('message-template');
-const locationMessageTemplate = document.getElementById('location-message-template');
-const sidebarTemplate = document.getElementById('sidebar-template');
+const messageTemplate = document.getElementById('message-template').innerHTML;
+const locationMessageTemplate = document.getElementById('location-message-template').innerHTML;
+const sidebarTemplate = document.getElementById('sidebar-template').innerHTML;
 
 // Options
 // console.log(new URL(window.location.href).searchParams.get('username'))
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true }); // Get params from URL
+
+const autoscroll = () => {
+    // New message element
+    const newMessage = messagesOutput.lastElementChild;
+
+    // Get height of the new message
+    const newMessagesStyle = getComputedStyle(newMessage); // Return style of an element
+    const newMessageMargin = parseInt(newMessagesStyle.marginBottom);
+    const newMessageHeight = newMessage.offsetHeight + newMessageMargin;
+
+    // Visible height
+    const visibleHeight = messagesOutput.offsetWidth;
+
+    // Height of messages container
+    const containerHeight = messagesOutput.scrollHeight;
+
+    // How far have I scrolled?
+    const scrollOffset = messagesOutput.scrollTop + visibleHeight;
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        messagesOutput.scrollTop = messagesOutput.scrollHeight;
+    }
+};
 
 const resetMessage = () => {
     messageFormTextarea.value = '';
@@ -23,27 +46,29 @@ const resetMessage = () => {
 };
 
 socket.on('message', (message) => {
-    const html = Mustache.render(messageTemplate.innerHTML, {
+    const html = Mustache.render(messageTemplate, {
         username: message.username,
         message: message.text,
         createdAt: moment(message.createdAt).format('h:mm:ss a')
     });
     messagesOutput.insertAdjacentHTML('beforeend', html);
+    autoscroll();
 });
 
 socket.on('locationMessage', (message) => {
-    const html = Mustache.render(locationMessageTemplate.innerHTML, {
+    const html = Mustache.render(locationMessageTemplate, {
         username: message.username,
         url: message.url,
         createdAt: moment(message.createdAt).format('h:mm:ss a')
     });
     messagesOutput.insertAdjacentHTML('beforeend', html);
+    autoscroll();
 });
 
 resetMessage();
 
 socket.on('roomData', ({ room, users }) => {
-    document.getElementById('sidebar').innerHTML = Mustache.render(sidebarTemplate.innerHTML, {room, users});
+    document.getElementById('sidebar').innerHTML = Mustache.render(sidebarTemplate, {room, users});
 });
 
 messageFormTextarea.addEventListener('keyup', e => {
